cdd4e074ae9d57abe406d3808741f73626f8d5dc
s-kondo-09 <shushu20011031@gmail.com>
2024-08-26 00:22:08 +0900

    Add functions finding zero

--- Oka_Dykhne.py
+++ Oka_Dykhne.py
@@ -14,6 +14,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 from scipy.integrate import quad
+from scipy.optimize import newton
 
 # parameter
 v = 1  # energy slope
@@ -64,6 +65,9 @@ def Hc(t, component):
     H['x_dot'] = v
     H['y_dot'] = k * v**2 * q(t)
     H['z_dot'] = 0
+    H['x_dot_dot'] = 0
+    H['y_dot_dot'] = k * v**2
+    H['z_dot_dot'] = 0
 
     return H[component]
 
@@ -82,19 +86,44 @@ def E_plus(t):
     return E_plus
 
 
-def phi_dot_approx(t):
+def phi_dot(t):
     """
-    approximate form of phi dot
+    define first derivative of phi
 
     Args:
         t (float): time
 
     Returns:
-        float: phi dot (approximated)
+        float: first derivative of phi
     """
     num = -Hc(t, "x")*Hc(t, "y_dot") + Hc(t, "x_dot")*Hc(t, "y")
     den = Hc(t, "x")**2 + Hc(t, "y")**2
     return num / den
+
+
+def phi_dot_dot(t):
+    """
+    define second derivative of phi
+
+    Args:
+        t (float): time
+
+    Returns:
+        float: second derivative of phi
+    """
+    X = Hc(tt + 1j*t, "x")
+    Y = Hc(tt + 1j*t, "y")
+    X_d = Hc(tt + 1j*t, "x_dot")
+    Y_d = Hc(tt + 1j*t, "y_dot")
+    X_dd = Hc(tt + 1j*t, "x_dot_dot")
+    Y_dd = Hc(tt + 1j*t, "y_dot_dot")
+
+    num_term1 = (X*Y_dd - Y*X_dd) * (X**2 + Y**2)
+    num_term2 = 2 * (X*Y_d - Y*X_d) * (X*X_d + Y*Y_d)
+    num = num_term1 - num_term2
+    den = (X**2 + Y**2)**2
+
+    return -num / den
 
 
 def Re_E(t):
@@ -110,14 +139,54 @@ def Re_E(t):
     X = Hc(tt + 1j*t, "x")
     Y = Hc(tt + 1j*t, "y")
     Z = Hc(tt + 1j*t, "z")
-    phi_dot = phi_dot_approx(tt + 1j*t)
-    a = X**2 + Y**2 + (Z + 0.5*(-F)*phi_dot)**2
+    phi_d = phi_dot(tt + 1j*t)
+    a = X**2 + Y**2 + (Z + 0.5*(-F)*phi_d)**2
     Integrand = (cmath.sqrt(a))
     return -4 * (-F) / abs(F) * Integrand.real
 
 
+def E_for_zero(t):
+    X = Hc(tt + 1j*t, "x")
+    Y = Hc(tt + 1j*t, "y")
+    Z = Hc(tt + 1j*t, "z")
+    phi_d = phi_dot(tt + 1j*t)
+
+    return X**2 + Y**2 + (Z + 0.5*(-F)*phi_d)**2
+
+
+def derv_E_for_zero(t):
+    """
+    derivative of adiabatic energy (unitary transformed) for finding zero
+
+    Note: 正確にはこの関数の戻り値の平方根が断熱エネルギーである。
+
+    Args:
+        t (float): time
+
+    Returns:
+        float: derivative of adiabatic energy
+    """
+    X = Hc(tt + 1j*t, "x")
+    Y = Hc(tt + 1j*t, "y")
+    Z = Hc(tt + 1j*t, "z")
+    X_dot = Hc(tt + 1j*t, "x_dot")
+    Y_dot = Hc(tt + 1j*t, "y_dot")
+    Z_dot = Hc(tt + 1j*t, "z_dot")
+    phi_d = phi_dot(tt + 1j*t)
+    phi_dd = phi_dot_dot(tt + 1j*t)
+
+    term1 = 2 * X * X_dot
+    term2 = 2 * Y * Y_dot
+    term3 = 2 * (Z - 0.5*(-F)*phi_d) * (Z_dot - 0.5*(-F)*phi_dd)
+
+    return term1 + term2 + term3
+
+
 for F in F_eval:
-    tt = 0  # transition time
+    tt = 0.01  # transition time
+    zero_approx = (m + k*v*abs(F)/4) / (v * (-F))
+    closest_zero = newton(E_for_zero, tt+1j*zero_approx, fprime=derv_E_for_zero, maxiter=1000)
+    print(closest_zero)
     zero_approx = (m + k*v*abs(F)/4) / (v * (-F))
     # zero of adiabatic energy (approximated)
 
@@ -132,3 +201,5 @@ for F in F_eval:
 plt.plot(F_eval, TP_list)
 plt.ylim(-0.1, 1.1)
 plt.show()
+
+# %%
