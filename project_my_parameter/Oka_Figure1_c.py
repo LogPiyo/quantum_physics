ca7adec50a252c83a3e006931ce893dedaa29e05
LogPiyo <shushu20011031@gmail.com>
2025-06-18 22:40:54 +0900

    fix: 型ヒント

--- project_my_parameter/Oka_Figure1_c.py
+++ project_my_parameter/Oka_Figure1_c.py
@@ -5,30 +5,33 @@
 # %%
 import _pathmagic # noqa
 
+from typing import Final
+import numpy.typing as npt
+
 import numpy as np
 import matplotlib.pyplot as plt
-
-from my_module.function import TLZ_theoretical, q, func_psi_module, eig_vec
 from scipy.integrate import solve_ivp
 
+from my_module.function import TLZ_theoretical, q, func_psi_module, eig_vec
+
 # parameter
-eps_0 = 1  # energy slope
-D_z = 0.1  # minimal energy gap
-D_y = 0.25  # twist strength
+eps_0: float = 1  # energy slope
+D_z: float = 0.1  # minimal energy gap
+D_y: float = 0.25  # twist strength
 
-h = 1  # Dirac constant
+h: Final[float] = 1  # Dirac constant
 
-n = 50  # step
-t_i = -10  # initial time
-t_f = 10  # final time
+n: int = 50  # step
+t_i: float = -10  # initial time
+t_f: float = 10  # final time
 
-TP_list = []  # transition probability
+TP_list: list[float] = []  # transition probability
 
-t_eval = np.linspace(-t_f, t_f, n)  # time
-F_values = np.linspace(-2, 2, n)  # sweep speed
+t_eval: npt.NDArray = np.linspace(-t_f, t_f, n)  # time
+F_values: npt.NDArray = np.linspace(-2, 2, n)  # sweep speed
 
 
-def H(t, component):
+def H(t: float, component: str) -> float:
     """
     define real Hamiltonian
 
@@ -39,7 +42,7 @@ def H(t, component):
     Returns:
         float: 時刻tにおけるcomponentで指定した成分
     """
-    H = {}
+    H: dict[str, float] = {}
 
     H['x'] = eps_0 * q(t, F)
     H['y'] = 0.5 * (4 * D_y / eps_0**2) * eps_0**2 * q(t, F)**2
@@ -51,29 +54,30 @@ def H(t, component):
     return H[component]
 
 
-def func_psi(t, var):
+def func_psi(t: float, var: list[float]) -> list[float]:
     return func_psi_module(t, H, var)
 
 
 # 各Fにおけるpsiの時間発展を計算する
 for F in F_values:
-    var_init_tmp = eig_vec(t_i, H, "upper").tolist()  # initial state
-    var_init = [var_init_tmp[0].real, var_init_tmp[0].imag,
-                var_init_tmp[1].real, var_init_tmp[1].imag]
+    var_init_tmp: list[float] = eig_vec(t_i, H, "upper").tolist()  # initial state
+    var_init: list[float] = [var_init_tmp[0].real, var_init_tmp[0].imag,
+                             var_init_tmp[1].real, var_init_tmp[1].imag]
     var_list = solve_ivp(func_psi, [t_i, t_f], var_init,
-                         method="LSODA", rtol=1e-12, atol=1e-12)
+                         method="LSODA", rtol=1e-12, atol=1e-12)  # 型はscipy.integrate.OdeResult
     # 各Fにおける固有関数とpsiを求め，トンネル確率TPを算出する
-    a = var_list.y[0][-1]
-    b = var_list.y[1][-1]
-    c = var_list.y[2][-1]
-    d = var_list.y[3][-1]
-    psi = np.array([[a+b*1j],
-                    [c+d*1j]])  # state vector
-    q_f = eig_vec(t_f, H, "lower")  # final state
-    dot = np.vdot(q_f, psi)
-    TP = abs(dot)**2  # transition probability
+    a: float = var_list.y[0][-1]
+    b: float = var_list.y[1][-1]
+    c: float = var_list.y[2][-1]
+    d: float = var_list.y[3][-1]
+    psi: npt.NDArray = np.array([[a+b*1j],
+                                 [c+d*1j]])  # state vector
+    q_f: npt.NDArray = eig_vec(t_f, H, "lower")  # final state
+    dot: float = np.vdot(q_f, psi)
+    TP: float = abs(dot)**2  # transition probability
     TP_list.append(TP)
 
+# %% グラフの描画
 plt.plot(F_values, TP_list, label="numerical")
 plt.plot(F_values, TLZ_theoretical(eps_0, F_values, D_z, (4 * D_y / eps_0**2)),
          linestyle=":", label="theoretical")
