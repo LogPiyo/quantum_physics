df3e81b707e164d4c90f7063e3cdfa4e4b3838fd
LogPiyo <shushu20011031@gmail.com>
2025-07-10 18:49:33 +0900

    fix: リファクタリング

--- project_my_parameter/TLZ_time_evolution.py
+++ project_my_parameter/TLZ_time_evolution.py
@@ -4,14 +4,18 @@
 # プログラムです。初期状態がlower stateのとき、upper stateの占有確率を求めます。
 
 # %%
+# self-made libraries
 import _pathmagic # noqa
+from my_module.function import q, TLZ_theoretical
+from my_module.calculator import calculate_occupation_probability
+
+# type hint libraries
+from numpy.typing import NDArray
+
+# math libraries
 import math
 import numpy as np
 import matplotlib.pyplot as plt
-
-from my_module.function import q
-from my_module.calculator import calculate_occupation_probability
-from numpy.typing import NDArray
 
 # parameter
 eps_0 = 1  # energy slope
@@ -25,36 +29,23 @@ n = 100  # step
 t_eval = np.linspace(t_i, t_f, n)  # time
 
 
-def H(t, component, real=True):
-    """
-    define real Hamiltonian
-
-    Args:
-        t (float): time
-        component (string): component of vector
-
-    Returns:
-        float: specified component
-    """
-    H = {}
-
-    H['x'] = eps_0 * q(t, F)
-    H['y'] = 0.5 * (4 * D_y / eps_0**2) * eps_0**2 * q(t, F)**2
-    H['z'] = D_z
-    H['x_dot'] = eps_0
-    H['y_dot'] = (4 * D_y / eps_0**2) * eps_0**2 * q(t, F)
-    H['z_dot'] = 0
-
-    return H[component]
+def H(t, component):
+    hamiltonian = {
+        'x': eps_0 * q(t, F),
+        'y': 0.5 * (4 * D_y / eps_0**2) * eps_0**2 * q(t, F)**2,
+        'z': D_z,
+        'x_dot': eps_0,
+        'y_dot': (4 * D_y / eps_0**2) * eps_0**2 * q(t, F),
+        'z_dot': 0
+    }
+    return hamiltonian[component]
 
 
-OP_list: NDArray = calculate_occupation_probability(H, t_i, t_f, n)
+occupation_prob_arr: NDArray = calculate_occupation_probability(H, t_i, t_f, n)
+transition_prob_arr: NDArray = TLZ_theoretical(eps_0, F, D_z, 4 * D_y / eps_0**2) + t_eval * 0
 
-TLZ = -math.pi * (D_z + (4 * D_y / eps_0**2)*eps_0*F/4)**2 / (abs(eps_0) * abs(F))
-P_TLZ = math.exp(TLZ) + t_eval*0  # theoretical
-arr = np.array(OP_list)
-plt.plot(t_eval, arr, label="numerical")
-plt.plot(t_eval, P_TLZ, label="theoretical")
+plt.plot(t_eval, occupation_prob_arr, label="numerical")
+plt.plot(t_eval, transition_prob_arr, label="theoretical")
 plt.ylim(-0.1, 1.1)
 plt.legend()
 plt.show()
